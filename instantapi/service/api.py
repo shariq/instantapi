0a9297ec401a8872981c6fb591b897b52bdecd77
Bobbie Chen <bobbiechen75@gmail.com>
2019-11-23 13:22:15 -0800

    Parameterize actions

--- instantapi/service/api.py
+++ instantapi/service/api.py
@@ -1,3 +1,4 @@
+import jinja2
 import json
 import requests
 
@@ -6,8 +7,10 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
 from worker.replayer import Pool, DummyWorker
+# from mock import Mock
 
 worker_pool = Pool()
+# worker_pool = Mock()  # use when just testing the API
 
 app = Flask(__name__, static_url_path="")
 db_path = Path(__file__).parent.parent.joinpath("database.db").absolute()
@@ -103,6 +106,26 @@ def invocation_error_callback(invocation_id):
     return callback
 
 
+def run_action(invocation, incoming_parameters: dict):
+    if invocation.action.parameters is not None:
+        content = invocation.action.content
+    else:
+        content = jinja2.Template(invocation.action.content).render(incoming_parameters)
+
+    worker_pool.async_run_job(
+        params={"id": invocation.id, "content": content},
+        start_callback=invocation_start_callback(invocation.id),
+        end_callback=invocation_end_callback(invocation.id),
+        error_callback=invocation_error_callback(invocation.id),
+    )
+    db.session.refresh(invocation)
+    return invocation.as_dict()
+
+def check_required_parameters(required: set, incoming: set):
+    if not required.issubset(incoming):
+        abort(400, message=f"Missing parameters: {list(required - incoming)}")
+
+
 class InvocationList(ListResource):
     model = Invocation
 
@@ -110,20 +133,14 @@ class InvocationList(ListResource):
         data = request.get_json(force=True)
         action_id = data["action_id"]
         action = Action.query.get_or_404(action_id)
+        if action.parameters is not None:
+            check_required_parameters(
+                set(action.parameters),
+                set(data['parameters'].keys())
+            )
         invocation_dict = super().post()
         invocation = Invocation.query.get_or_404(invocation_dict["id"])
-
-        # TODO: template invocation parameters into action.content
-        # Maybe needs to be it's own "templater" thing
-
-        worker_pool.async_run_job(
-            params={"id": invocation.id, "content": action.content,},
-            start_callback=invocation_start_callback(invocation.id),
-            end_callback=invocation_end_callback(invocation.id),
-            error_callback=invocation_error_callback(invocation.id),
-        )
-        db.session.refresh(invocation)
-        return invocation.as_dict()
+        return run_action(invocation, data['parameters'])
 
 
 class ActionInvocationList(Resource):
@@ -133,18 +150,17 @@ class ActionInvocationList(Resource):
         return jsonify(items=[m.as_dict() for m in invocations])
 
     def post(self, action_id):
-        Action.query.get_or_404(action_id)
-        invocation = Invocation(action_id=action_id)
-        db.session.add(new)
+        data = request.get_json(force=True)
+        action = Action.query.get_or_404(action_id)
+        if action.parameters is not None:
+            check_required_parameters(
+                set(action.parameters),
+                set(data['parameters'].keys())
+            )
+        invocation = Invocation(action_id=action_id, parameters=data['parameters'])
+        db.session.add(invocation)
         db.session.commit()
-        worker_pool.async_run_job(
-            params={"id": invocation.id, "content": action.content,},
-            start_callback=invocation_start_callback(invocation.id),
-            end_callback=invocation_end_callback(invocation.id),
-            error_callback=invocation_error_callback(invocation.id),
-        )
-        db.session.refresh(invocation)
-        return invocation.as_dict()
+        return run_action(invocation, data['parameters'])
 
 
 @app.route("/screenshots/<path:path>")
